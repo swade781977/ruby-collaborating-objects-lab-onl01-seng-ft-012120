4f042f5f866e99f8e5da64846d32bde595497e3e
Chris Wade <swade781977@yahoo.com>
2020-02-07 02:03:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,11 @@ class Song
   
   def self.new_by_filename(file)
     file = file.split(" - ")
-    a = "#{file[0]}"
+    a = Song.new(file[1])
+    binding.pry
+    a = file[0]
     artist_name=(a) 
-     a = Song.new("#{file[1]}")
+     
      a.intern
     genre = "#{file[2].gsub!(".mp3", "")}"
   end
