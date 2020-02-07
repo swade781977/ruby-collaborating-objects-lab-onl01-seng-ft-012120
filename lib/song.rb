3843a38587290ccebad2966d4f2d4d4d074378cc
Chris Wade <swade781977@yahoo.com>
2020-02-07 01:32:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Song 
+  @@all = []
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.new_by_filename(file)
+    file = file.split(" - ")
+    file[2] = file[2].gsub!(".mp3", "")
+    a = artist_name=(file[0]) 
+    binding.pry
+    
+  end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    
+  end
+end
